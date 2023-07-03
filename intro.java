88780eca3f5a6ebe36f07076e0afc11f872832af
Vikas181119 <viksp181119@gmail.com>
2023-07-03 09:29:19 +0530

    first commit

---
+++ intro.java
@@ -0,0 +1,10 @@
+class Output {
+    public static void main(String[] args) {
+    	
+        System.out.println("Vikas ");
+        System.out.println("Sharma ");
+    	
+        System.out.print("vinsys ");
+        System.out.print("javafsd");
+    }
+}
